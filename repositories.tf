bdc589ec420f091bc98bae3ba4b21ff5e78098b3
Scott R Bowe <scottb@sentania.net>
2025-12-10 14:39:47 -0600

    i hate how all my troubleshooting is tracked in git

--- repositories.tf
+++ repositories.tf
@@ -3,7 +3,7 @@ locals {
     for k, p in module.projects :
     k => {
       project_name = p.project_name
-      project_id   = p.project_id
+      project_id   = p.id
 
       project_slug = trim(
         regexreplace(
@@ -14,7 +14,7 @@ locals {
         "-"
       )
 
-      project_id_short = substr(p.project_id, 0, 12)
+      project_id_short = substr(p.id, 0, 12)
 
       project_key = format(
         "%s__%s",
@@ -49,7 +49,7 @@ module "repository" {
   plaintext_secrets = {
     VCFA_PROJECT_NAME      = each.value.project_name
     VCFA_PROJECT_KEY       = each.value.project_key
-    VCFA_PROJECT_ID        = each.value.project_id
+    VCFA_PROJECT_ID        = each.value.id
     VCFA_ORGANIZATION_NAME = var.vcfa_organization
   }
 }
