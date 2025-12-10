f5551144d0c094d5cfc2ac4dbe253a3c8513d67c
Scott R Bowe <scottb@sentania.net>
2025-12-10 13:16:01 -0600

    recreate and migrate state to s3

--- backend.tf
+++ backend.tf
@@ -1,4 +1,9 @@
 terraform {
-  backend "pg" {
+  backend "s3" {
+    bucket         = "sentania-labs-terraform-state"
+    key            = "vra/vm-apps-private-cloud/lab/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
